705d386a2c502d3435a50051ac6e996757a3f972
Colt <39076891+LuckFire@users.noreply.github.com>
2024-01-09 20:24:33 -0500

    refactor(breaking): reorder param
    you can leave optional params empty if you need to set a scope without params

--- src/lib/OrderedDataStore.ts
+++ src/lib/OrderedDataStore.ts
@@ -39,10 +39,10 @@ export class OrderedDataStore {
      * Returns a list of entries from an ordered data store.
      * @link https://create.roblox.com/docs/reference/cloud/datastores-api/ordered-v1#GET-v1-universes-_universeId_-orderedDataStores-_orderedDataStore_-scopes-_scope_-entries
      * @param orderedDataStore The name of the target ordered data store.
-     * @param scope The range of qualifying values of entries to return. Default is `global`
      * @param optionalParams Optional params the endpoint will accept.
+     * @param scope The range of qualifying values of entries to return. Default is `global`
      */
-    public async listEntries(orderedDataStore: string, scope: string = "global", optionalParams?: ListEntriesParams) {
+    public async listEntries(orderedDataStore: string, optionalParams?: ListEntriesParams, scope: string = "global") {
         const url = new URL(`/ordered-data-stores/v1/universes/${this.universeId}/orderedDataStores/${orderedDataStore}/scopes/${scope}/entries`, baseApiUrl);
         if (optionalParams) addParams(url, optionalParams);
 
