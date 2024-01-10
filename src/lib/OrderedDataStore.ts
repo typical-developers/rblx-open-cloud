ba3398b73217e47594c80853f7c4b034a50cf06d
Colt <39076891+LuckFire@users.noreply.github.com>
2024-01-09 20:11:20 -0500

    chore: layout TODOs

--- src/lib/OrderedDataStore.ts
+++ src/lib/OrderedDataStore.ts
@@ -1,6 +1,26 @@
-import { ListEntries, ListEntriesParams } from "../types/OrderedDataStoreTypes";
+import { Entry, ListEntries, ListEntriesParams } from "../types/OrderedDataStoreTypes";
 import { baseApiUrl } from "../util/constants";
 import { addParams } from "../util/params";
+
+class OrderedDataStoreEntry {
+    public readonly datastore: Entry;
+    // Will turn this into a separate type later.
+    public readonly context: {
+        url: URL,
+        base: URL,
+        apiKey: string
+    };
+
+    // TODO: https://create.roblox.com/docs/reference/cloud/datastores-api/ordered-v1#DELETE-v1-universes-_universeId_-orderedDataStores-_orderedDataStore_-scopes-_scope_-entries-_entry_
+    public async delete() {
+
+    }
+
+    // TODO: https://create.roblox.com/docs/reference/cloud/datastores-api/ordered-v1#PATCH-v1-universes-_universeId_-orderedDataStores-_orderedDataStore_-scopes-_scope_-entries-_entry_
+    public async update() {
+
+    }
+}
 
 export class OrderedDataStore {
     readonly apiKey: string;
@@ -19,10 +39,10 @@ export class OrderedDataStore {
      * Returns a list of entries from an ordered data store.
      * @link https://create.roblox.com/docs/reference/cloud/datastores-api/ordered-v1#GET-v1-universes-_universeId_-orderedDataStores-_orderedDataStore_-scopes-_scope_-entries
      * @param orderedDataStore The name of the target ordered data store.
-     * @param scope The range of qualifying values of entries to return.
+     * @param scope The range of qualifying values of entries to return. Default is `global`
      * @param optionalParams Optional params the endpoint will accept.
      */
-    public async list(orderedDataStore: string, scope: string, optionalParams?: ListEntriesParams) {
+    public async listEntries(orderedDataStore: string, scope: string = "global", optionalParams?: ListEntriesParams) {
         const url = new URL(`/ordered-data-stores/v1/universes/${this.universeId}/orderedDataStores/${orderedDataStore}/scopes/${scope}/entries`, baseApiUrl);
         if (optionalParams) addParams(url, optionalParams);
 
@@ -32,9 +52,13 @@ export class OrderedDataStore {
         return json;
     }
 
-    public async create(orderedDataStore: string, scope: string, id: string) {
-        const url = new URL(`/ordered-data-stores/v1/universes/${this.universeId}/orderedDataStores/${orderedDataStore}/scopes/${scope}/entries`, baseApiUrl);
+    // TODO: https://create.roblox.com/docs/reference/cloud/datastores-api/ordered-v1#POST-v1-universes-_universeId_-orderedDataStores-_orderedDataStore_-scopes-_scope_-entries
+    public async createEntry(orderedDataStore: string, scope: string, id: string) {
+
+    }
+
+    // TODO: https://create.roblox.com/docs/reference/cloud/datastores-api/ordered-v1#GET-v1-universes-_universeId_-orderedDataStores-_orderedDataStore_-scopes-_scope_-entries-_entry_
+    public async getEntry() {
         
-        url.searchParams.set('id', id);
     }
 }
