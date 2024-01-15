f269cede7d7cb55ffdce5f4055addd41c1e96004
Colt <39076891+LuckFire@users.noreply.github.com>
2024-01-15 16:49:56 -0500

    feat: explode type classes
    there are some scenarios where you will need the type for these classes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 export * from './structures/MessagingService';
 export * from './structures/DataStores/DataStoreService';
+
+export { type DataStore } from './structures/DataStores/DataStore';
+export { type DataStoreEntry } from './structures/DataStores/DataStoreEntry';
+export { type DataStoreKeyPage } from './structures/DataStores/DataStoreKeyPage';
+export { type DataStoreListingPage } from './structures/DataStores/DataStoreListingPage';
+export { type DataStoreVersionPages } from './structures/DataStores/DataStoreVersionPage';
+export { type OrderedDataStoreEntry } from './structures/DataStores/OrderedDataStoreEntry';
